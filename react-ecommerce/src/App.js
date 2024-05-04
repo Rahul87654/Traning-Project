ff98b09f42f280ca27fc1887a84f3a3dbf3246a9
Rahulgangal0013 <rahulgangal31@gmail.com>
2024-05-05 02:33:44 +0530

    Added Routes and Created the Sign up and Login Page

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import { Counter } from './features/counter/Counter';
 import './App.css';
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom';
 import Home from './features/Pages/Home';
+import LoginPage from './features/Pages/LoginPage';
+import SignupPage from './features/Pages/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage/>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage/>,
+  },
+]);
+
 function App() {
   return (
-   <>
-     <Home/>
-   </>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
